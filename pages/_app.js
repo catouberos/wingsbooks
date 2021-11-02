345d31e9ec1766fd64cf2dd2864b8ef5fe207512
Khoa Nguyen Do <khoanguyen.do@outlook.com>
2021-11-02 19:28:31 +0700

    ao-buta init

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
 import 'tailwindcss/tailwind.css'
+import '@splidejs/splide/dist/css/splide.min.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Component {...pageProps} />
+  )
 }
 
 export default MyApp
